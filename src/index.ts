cc54ba51fb843cda60942d7c8423279ebe4ec4cd
Dries de Kort <dries.dekort@axxes.com>
2024-07-30 14:49:51 +0200

    add label to new pr

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import { getInput } from "@actions/core";
-const name = getInput('name');
-console.log(`Hello ${name}`);
+import { getInput, setFailed } from "@actions/core";
+import {context, getOctokit} from '@actions/github';
+async function run() {
+  const token = getInput('gh-token');
+  const label = getInput('label');
+
+  const octokit = getOctokit(token);
+  const pullRequest = context.payload.pull_request;
+
+  try {
+    if (!pullRequest){
+      throw new Error('this action can only be run on Pull Requests')
+    }
+
+    await octokit.rest.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest.number,
+      labels: [label],
+    });
+  } catch (error) {
+    setFailed( (error as Error)?.message ?? 'Unknown error')
+  }
+  
+}
+run();
